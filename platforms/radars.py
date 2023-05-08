b5e13bb1b6a58416c114085db12cb25543f8c3c2
yxhuang7538 <yxhuang7538@163.com>
2023-05-08 20:05:46 +0800

    编写Label-GM-PHD，并优化代码

--- platforms/radars.py
+++ platforms/radars.py
@@ -1,99 +1,127 @@
-# 生成不同的场景例子
-from typing import Dict, List
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''
+@File    :   radars.py
+@Time    :   2023/05/08 10:08:48
+@Author  :   yxhuang 
+@Desc    :   雷达类
+'''
+
 import numpy as np
+import yaml
+from rfs.distributions import Poisson
+from typing import List
 import random
-import math
-from platforms.targets import Target
-import matplotlib.pyplot as plt
 
 
 class Radar:
-    def __init__(self, pos: np.ndarray, id: int, T_s: float):
+    def __init__(self, pos: np.ndarray, id: int, T_s: float) -> None:
+        """雷达类
+
+        Args:
+            pos (np.ndarray): 雷达位置
+            id (int): 雷达编号
+            T_s (float): 雷达采样频率
+        """
         self.pos = pos  # 雷达位置
-        self.id = id  # 雷达的编号
+        self.id = id    # 雷达的编号
         self.T_s = T_s  # 雷达的采样周期
 
-        # --- 量测范围参数 ---#
-        self.R_min = 0
-        self.R_max = 60000
-        self.Azi_min = 0 * np.pi / 180
-        self.Azi_max = 360 * np.pi / 180
+        self.p_d = 0.99 # 检测概率
 
-        self.p_s = 0.99  # 目标存活概率
-        self.p_d = 0.98  # 检测概率
+        self.R = np.zeros([2, 2])           # 量测噪声协方差矩阵（量测为极坐标）
+        self.Q = np.zeros([4, 4])           # 过程噪声协方差矩阵
 
-        self.dim_z = 2  # 量测维度
-        self.H = np.array([[1, 0, 0, 0],
-                           [0, 1, 0, 0]])  # 观测矩阵
-        self.sigma_R = random.randint(5, 8)  # 距离标准差
-        self.sigma_Azi = random.uniform(0.1, 0.3) * np.pi / 180  # 方位角标准差
-        self.R = np.array([[self.sigma_R ** 2, 0], [0, self.sigma_Azi ** 2]])  # 量测噪声协方差矩阵
+        # 场景范围
+        self.r_min = 0                                # 距离范围
+        self.r_max = 2500
+        self.theta_min = -180 * np.pi / 180              # 方位角范围
+        self.theta_max = 180 * np.pi / 180
 
-        Q = np.zeros((4, 4))
-        I_2 = np.eye(2)
-        Q[0:2, 0:2] = (self.T_s ** 4) / 4 * I_2
-        Q[0:2, 2:] = (self.T_s ** 3) / 2 * I_2
-        Q[2:, 0:2] = (self.T_s ** 3) / 2 * I_2
-        Q[2:, 2:] = (self.T_s ** 2) * I_2
-        # 过程噪声
-        sigma_w = 5.
-        self.Q = Q * (sigma_w ** 2)
+        self.all_meas = []                            # 所有量测
+    
+    def config(self, cfg_path: str) -> None:
+        """加载配置文件，更改默认参数
 
-    def gen_all_meas(self, targets: List[List[np.ndarray]], lc: int) -> List[List[np.ndarray]]:
+        Args:
+            cfg_path (str): 配置文件路径
         """
-        产生所有时刻的量测值
-        :param lc: 杂波
-        :param targets: 每个时刻的目标真值
-        :return: 量测集合
+        with open(cfg_path, 'r') as stream:
+            cfg = yaml.safe_load(stream)
+
+        self.T_s, self.p_d = cfg['T_s'], cfg['p_d']
+
+        # 设置观测噪声协方差矩阵
+        sigma_r, sigma_theta = cfg['sigma_r'], cfg['sigma_theta']
+        self.set_R(sigma_r, sigma_theta)
+
+        # 设置过程噪声协方差矩阵
+        sigma_v = cfg['sigma_v']
+        self.set_Q(sigma_v)
+
+        # 场景范围
+        self.r_min, self.r_max = cfg['radar_r_min'], cfg['radar_r_max'] # 距离范围
+        self.theta_min = cfg['radar_theta_min'] * np.pi / 180     # 方位角范围 弧度
+        self.theta_max = cfg['radar_theta_max'] * np.pi / 180
+
+    def set_R(self, sigma_r: float, sigma_theta: float) -> None:
+        """生成观测噪声协方差矩阵
+
+        Args:
+            sigma_r (float): 距离标准差 （米）
+            sigma_theta (float): 方位标准差 （角度）
         """
-        meas = []
-        for X in targets:
-            m = []
-            for state in X:
-                if np.random.rand() <= self.p_d:  # 目标存活 被检测到
-                    z = self.cart2pol(state) + np.random.multivariate_normal(np.zeros(self.H.shape[0]),
-                                                                             self.R)
-                    m.append(z)
+        sigma_theta *= (np.pi / 180)    # 转弧度
+        self.R = np.eye(2) * np.array([sigma_r ** 2, sigma_theta ** 2])
+
+    def set_Q(self, sigma_v: float) -> None:
+        """设置过程噪声协方差矩阵
+
+        Args:
+            sigma_v (float): 速度标准差
+        """
+        q = np.array([[self.T_s ** 4 / 4, self.T_s ** 3 / 2],
+                       [self.T_s ** 3 / 2, self.T_s ** 2]])
+        zeros = np.zeros([2, 2])
+        self.Q = np.block([[q, zeros], [zeros, q]]) * (sigma_v ** 2)
+
+    def gen_all_trajs_meas(self, trajs: List[List[np.ndarray]], kz: Poisson) -> List[List[np.ndarray]]:
+        """为所有轨迹生成量测
+
+        Args:
+            trajs (List[List[np.ndarray]]): 所有目标
+            kz (Poisson): 杂波函数
+
+        Returns:
+            List[List[np.ndarray]]: 量测集合
+        """
+        all_meas = []
+        for traj in trajs:  # 取k时刻所有目标轨迹点
+            meas = []
+            for state_k in traj:
+                if np.random.rand() <= self.p_d:    # 目标存活，被检测到
+                    z = self.cart2pol(state_k) + np.random.multivariate_normal(np.zeros(2), self.R)
+                    meas.append(z)
             # 加入杂波
-            for i in range(np.random.poisson(lc)):
-                R = random.uniform(self.R_min, self.R_max)
-                Azi = random.uniform(self.Azi_min, self.Azi_max)
-                m.append(np.array([R, Azi]))
-            meas.append(m)
-
-        return meas
-
+            for _ in range(int(kz.rvs())):
+                r = random.uniform(self.r_min, self.r_max)
+                theta = random.uniform(self.theta_min, self.theta_max)
+                meas.append(np.array([r, theta]))
+            all_meas.append(meas)   # k时刻所有量测
+        self.all_meas = all_meas
+        return all_meas
+    
     def cart2pol(self, state: np.ndarray) -> np.ndarray:
-        R = np.linalg.norm(self.pos - state[0:2])
-        # Azi = (90 - math.degrees(math.atan2(state[1] - self.pos[1],
-        #                                     state[0] - self.pos[0])) + 360) % 360
-        # Azi = Azi * np.pi / 360
-        Azi = np.arctan2(state[1] - self.pos[1], state[0] - self.pos[0])
+        r = np.linalg.norm(self.pos - state[[0, 3]])
+        theta = np.arctan2(state[3] - self.pos[1], state[0] - self.pos[0])
 
-        return np.array([R, Azi])
+        return np.array([r, theta])
 
     def pol2cart(self, z: np.ndarray) -> np.ndarray:
         tmp = [z[0] * np.cos(z[1]), z[0] * np.sin(z[1])]
         tmp = np.array(tmp)
         return tmp
 
-    def plot_all_meas(self, meas):
-        plt.figure()
-        for zk in meas:
-            for z in zk:
-                z_cart = self.pol2cart(z)
-                plt.scatter(z_cart[0], z_cart[1], c='blue')
-        plt.show()
-
-
-if __name__ == "__main__":
-    radar = Radar(np.array([1, 2]), 1, 1)
-    target = Target(0, 100, 1, 1)
-    traj = target.genTraj()
-    target.plotTraj()
-    targets = []
-    [targets.append([traj[i, :]]) for i in range(traj.shape[0])]
-
-    meas = radar.gen_all_meas(targets, 10)
-
-    radar.plot_all_meas(meas)
+    
+    
+    
--- platforms/targets.py
+++ platforms/targets.py
@@ -1,9 +1,68 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''
+@File    :   targets.py
+@Time    :   2023/05/08 11:38:04
+@Author  :   yxhuang 
+@Desc    :   目标类
+'''
+
 import numpy as np
 from typing import List
 import random
-from platforms.ssm import SSM
 import matplotlib.pyplot as plt
 
+# 运动模型库
+
+import numpy as np
+import math
+import random
+
+
+class SSM:
+    def __init__(self, dim, T):
+        self.dim = dim  # 状态的维数 [x,vx,ax,y,vy,ay]
+        self.T = T  # 时间间隔
+        self.numsF = 3  # 运动状态个数
+
+    def CV(self):
+        if self.dim == 6:  # 2D
+            F = np.array([[1, self.T, 0, 0, 0, 0],
+                          [0, 1, 0, 0, 0, 0],
+                          [0, 0, 0, 0, 0, 0],
+                          [0, 0, 0, 1, self.T, 0],
+                          [0, 0, 0, 0, 1, 0],
+                          [0, 0, 0, 0, 0, 0]])
+            return F
+
+    def CA(self):
+        if self.dim == 6:  # 2D
+            F = np.array([[1, self.T, math.pow(self.T, 2) / 2, 0, 0, 0],
+                          [0, 1, self.T, 0, 0, 0],
+                          [0, 0, 1, 0, 0, 0],
+                          [0, 0, 0, 1, self.T, math.pow(self.T, 2) / 2],
+                          [0, 0, 0, 0, 1, self.T],
+                          [0, 0, 0, 0, 0, 1]])
+            return F
+
+    def CT(self, w):
+        if self.dim == 6:  # 2D
+            F = np.array([[1, math.sin(w * self.T) / w, 0, 0, (math.cos(w * self.T) - 1) / w, 0],
+                          [0, math.cos(w * self.T), 0, 0, -math.sin(w * self.T), 0],
+                          [0, 0, 1, 0, 0, 0],
+                          [0, (1 - math.cos(w * self.T)) / w, 0, 1, math.sin(w * self.T) / w, 0],
+                          [0, math.sin(w * self.T), 0, 0, math.cos(w * self.T), 0],
+                          [0, 0, 0, 0, 0, 1]])
+            return F
+
+    def selectOneF(self, w):
+        flag = random.randint(1, self.numsF)
+        if flag == 1:
+            return self.CV()
+        elif flag == 2:
+            return self.CA()
+        else:
+            return self.CT(w)
 
 class Target:
     def __init__(self, birth_time: int, death_time: int, dt: float, label: int):
@@ -17,8 +76,8 @@ class Target:
         self.dim_x = 6
         self.state = np.zeros([self.seq_len,
                                self.dim_x])  # 目标状态
-        self.accRange = [-3, 3]  # 加速度范围
-        self.posRange = [-50000, 50000]  # 位置范围（针对的是目标的出生位置）
+        self.accRange = [-0.03, 0.03]  # 加速度范围
+        self.posRange = [-2000, 2000]  # 位置范围（针对的是目标的出生位置）
         self.vRange = [-15, 15]  # 速度范围
         self.wRange = [-10, 10]  # 转向率范围
 
@@ -43,7 +102,8 @@ class Target:
             ay = random.uniform(self.accRange[0], self.accRange[1])
             a = [ax, ay]
 
-        state = np.array([pos[0], pos[1], v[0], v[1], a[0], a[1]]).T    # 初始状态
+        # state = np.array([pos[0], pos[1], v[0], v[1], a[0], a[1]]).T    # 初始状态
+        state = np.array([pos[0], v[0], a[0], pos[1], v[1], a[1]]).T
         return state
 
     def genTraj(self):
@@ -62,7 +122,7 @@ class Target:
             # TODO 探索一下转移噪声
             if k == maneuvering_t:
                 F = ssm.selectOneF(w)
-            self.state[k, :] = np.dot(F, self.state[k - 1, :])
+            self.state[k, :] = F @ self.state[k - 1, :]
 
         Traj = self.state
         return Traj
@@ -73,8 +133,8 @@ class Target:
         :return:
         """
         plt.figure()
-        plt.plot(self.state[:, 0], self.state[:, 1], color='blue', marker='o', linestyle='dashed')
-        plt.show()
+        plt.plot(self.state[:, 0], self.state[:, 3], color='blue', marker='o', linestyle='dashed')
+        plt.savefig("test.png")
 
     def getOnePoints(self, k):
         """
@@ -90,8 +150,37 @@ class Target:
         :param k:
         :return:
         """
-        return [self.state[k, 0], self.state[k, 1]]
+        return [self.state[k, 0], self.state[k, 3]]
 
+
+def gen_batch_targets(batch: int, K=100, label_start=1, T_s=1) -> List:
+    """批量生成目标
+
+    Args:
+        batch (int): 批量大小
+        K (int, optional): 观测时长. Defaults to 100.
+        label_start (int, optional): 起始标签. Defaults to 1.
+        T_s (int, optional): 观测频率. Defaults to 1.
+
+    Returns:
+        List: 轨迹list和目标对象list
+    """
+    trajs = []
+    [trajs.append([]) for _ in range(K)]
+    targets = []
+    for _ in range(batch):
+        birth_time = random.randint(0, int(0.2 * K))
+        death_time = random.randint(int(0.8 * K), K)
+        target = Target(birth_time,
+                        death_time,
+                        T_s,
+                        label_start)
+        traj = target.genTraj()
+        targets.append(target)
+        # target.plotTraj()
+        [trajs[j].append(traj[j]) for j in range(traj.shape[0])]
+
+    return [trajs, targets]
 
 if __name__ == "__main__":
     target = Target(0, 100, 1, 1)
